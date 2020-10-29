0a7ca4a7bd7f034748d35a3d04b6b25dc4ecffbf
radevich-a <radevich-aa@yandex.ru>
2020-10-29 22:23:11 +0300

    add Main

---
+++ MyArrayDataException.java
@@ -0,0 +1,13 @@
+public class MyArrayDataException extends RuntimeException{
+
+    public MyArrayDataException() {
+    }
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
