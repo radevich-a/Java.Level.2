1258e16ca3a5d39120a12eac5c0c6d63168748c1
radevich-a <radevich-aa@yandex.ru>
2020-10-29 22:23:11 +0300

    add Classes

---
+++ MyArraySizeException.java
@@ -0,0 +1,12 @@
+public class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException() {
+    }
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+
+    public MyArraySizeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
